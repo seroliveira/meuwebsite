847c11c500685692f4ab4e5d84a50048b50364c4
Sérgio Oliveira <sergioliveira90@gmail.com>
2025-06-27 16:32:41 +0100

    atualizações mobile + pag hobbies

--- hobbies.php
+++ hobbies.php
@@ -5,7 +5,96 @@ include("header.php");
 ?>
 
 <!-- conteúdo do body -->
- 
+ <div class="container">
+    <p>Uma tabela? Ah pois é!!</p>
+    <p>O que vou colocar aqui? Não faço ideia. Neste momento só quero ter mais conteúdo no website e decidi implementar algo que demos na última sessão</p>
+    <p>Está bonito? Não! Também, nunca fui bom a design. Mas o conteúdo está cá!</p>
+    <table class="hobbies">
+        <thead>
+            <tr>
+                <th colspan="3">Hoobies</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Música</td>
+                <td>Rock</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Gaming</td>
+                <td>RPG / J-RPG</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Programar</td>
+                <td>Ainda muito verde</td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+
+    <p>Como assim, não chega?</p>
+    <p>Querias ver o quê? Uma pirâmide? Estranho mas.. ok!</p>
+
+    <table class="piramide">
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><img src="img/cromo.png" alt="cromo"></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><img src="img/cromo.png" alt="cromo"></td>
+            <td></td>
+            <td><img src="img/cromo.png" alt="cromo"></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td><img src="img/cromo.png" alt="cromo"></td>
+            <td></td>
+            <td><img src="img/cromo.png" alt="cromo"></td>
+            <td></td>
+            <td><img src="img/cromo.png" alt="cromo"></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><img src="img/cromo.png" alt="cromo"></td>
+            <td></td>
+            <td><img src="img/cromo.png" alt="cromo"></td>
+            <td></td>
+            <td><img src="img/cromo.png" alt="cromo"></td>
+            <td></td>
+            <td><img src="img/cromo.png" alt="cromo"></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td><img src="img/cromo.png" alt="cromo"></td>
+            <td></td>
+            <td><img src="img/cromo.png" alt="cromo"></td>
+            <td></td>
+            <td><img src="img/cromo.png" alt="cromo"></td>
+            <td></td>
+            <td><img src="img/cromo.png" alt="cromo"></td>
+            <td></td>
+            <td><img src="img/cromo.png" alt="cromo"></td>
+        </tr>
+    </table>
+ </div>
 
 <!--rodapé-->
 <?php include("footer.php"); ?>
